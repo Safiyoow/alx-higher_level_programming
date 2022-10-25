02f07e897bafc789ea81fd0cbb295374cc8dce27
Safiyoow <izabelasharif@gmail.com>
2022-10-25 03:32:16 -0700

    p

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,16 +1,3 @@
 #!/usr/bin/python3
-"""
-Module: 1-my_list
-This is a class that inherits from list
-It prints the list, but sorted (ascending sort)
-"""
-
-
-class MyList(list):
-    """
-    A function that print the list in sorted ascending order
-    """
-    def print_sorted(self):
-        new_list = self[:]
-        new_list.sort()
-        print("{}".format(new_list))
+def is_same_class(obj, a_class):
+    return type(obj) == a_class
